82347be4231c9e337dbb59715fb19ea1716c051a
Patricia <prmonday@gmail.com>
2020-11-23 19:15:47 +0000

    firts test passed

--- lib/piggy_bank.rb
+++ lib/piggy_bank.rb
@@ -1,24 +1,2 @@
 class PiggyBank
-
-def initialize
-  @coins = 0
-
-end
-
-  def insert(sum) # I could put insert as attr_writer when refactoring
-    @coins = @coins + sum
-  end
-
-  def count
-    @coins
-
-  end
-
-  def check
-    return 'cling'
-  end
-
-  def break
-    @coins = 8
-  end
-end
+end 
--- spec/piggy_bank_spec.rb
+++ spec/piggy_bank_spec.rb
@@ -1,47 +1,8 @@
 require 'piggy_bank'
 
 describe 'PiggyBank' do
-  it 'can save coins in a piggy bank' do
-    save = PiggyBank.new
-    expect(save).to be_kind_of(PiggyBank)
-  end
-
-
-  it 'can have coins inserted'do
-    save = PiggyBank.new
-    save.insert(0)
-    expect(save).to respond_to(:insert).with(1).argument
-  end
-
-
-  it 'can count the coins inserted' do
-    save = PiggyBank.new
-    save.insert(5)
-    save.insert(2)
-    expect(save.count).to eq(7)
-  end
-  it 'will make a sound with coins in it.' do
-   save = PiggyBank.new
-   save.insert(8)
-   expect(save.check).to eq('cling')
-  end
-
-  it 'will break and count the money in it.' do
-    save = PiggyBank.new
-    save.insert(8)
-    expect(save.break).to eq(8)
+  it 'saves coins into a piggy bank' do
+  saves = PiggyBank.new
+  expect(saves).to be_instance_of(PiggyBank)
   end
 end
-
-
-
-
-#
-
-
-
-#2.step : Insert 5 coins  into the piggy bank
-#3.step: Check if theres 5 coins
-#4.step: New scenario, the piggy bank has 5 coins and I've inserted 2 more.
-
-# instance variable they can store value and it will be private value
