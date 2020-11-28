1a90a1c2eece965d33ce2996dd481cc54b86161c
Patricia <prmonday@gmail.com>
2020-11-28 12:48:59 +0000

    second test

--- lib/piggy_bank.rb
+++ lib/piggy_bank.rb
@@ -1,8 +1,2 @@
 class PiggyBank
-  def insert(input_coins)
-  end
-
-  def shake
-    'cling'
-  end 
-end
+end 
--- spec/piggy_bank_spec.rb
+++ spec/piggy_bank_spec.rb
@@ -1,18 +1,15 @@
 require 'piggy_bank'
 
 describe 'PiggyBank' do
-  it 'saves coins into a piggy bank' do
-    piggy_bank = PiggyBank.new
+  it 'takes coins and keeps saved in a piggy bank'do
+  piggy_bank = PiggyBank.new
   expect(piggy_bank).to be_instance_of(PiggyBank)
-  end
-  it 'takes coins into a piggy bank' do
-    piggy_bank = PiggyBank.new
-    piggy_bank.insert(0)
-    expect(piggy_bank).to respond_to(:insert).with(1).argument
-  end
-  it 'I can check if there are coins in it I can shake and it will "cling"'do
-    piggy_bank = PiggyBank.new
-    piggy_bank.shake
-    expect(piggy_bank.shake).to eq('cling')
   end 
 end
+
+#input          output
+
+# coins          coins insert in a piggy bank
+#insert coins    ammount
+# shake          "cling"
+# count           ammount
