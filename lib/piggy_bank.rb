813cc94bc37df81efa0dc5a6f22abc8f7720bebb
Patricia <prmonday@gmail.com>
2020-12-04 18:21:26 +0000

    3 cases

--- lib/piggy_bank.rb
+++ lib/piggy_bank.rb
@@ -1,2 +1,9 @@
 class PiggyBank
-end 
+  def initialize(input_coins)
+     @input_coins = 0
+  end
+def amount
+  @input_coins + 5
+end
+
+end
--- spec/piggy_bank_spec.rb
+++ spec/piggy_bank_spec.rb
@@ -4,12 +4,23 @@ describe 'PiggyBank' do
   it 'takes coins and keeps saved in a piggy bank'do
   piggy_bank = PiggyBank.new
   expect(piggy_bank).to be_instance_of(PiggyBank)
-  end 
+  end
+   it 'takes coins inserted.'do
+     piggy_bank = PiggyBank.new
+     piggy_bank.amount(0)
+     expect(piggy_bank).to respond_to(:amount).with(1).argument
+   end
+   it 'can take 5 coins'do
+     piggy_bank = PiggyBank.new
+     piggy_bank.amount(5)
+     expec(piggy_bank.amount).to eq(5)
+   end
 end
 
 #input          output
 
 # coins          coins insert in a piggy bank
-#insert coins    ammount
+# zero coin       0
+# insert 5 coins  5
 # shake          "cling"
 # count           ammount
