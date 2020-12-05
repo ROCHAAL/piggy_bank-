17546a3d7397524b865a1e82d6d7b699b37b29e9
Patricia <prmonday@gmail.com>
2020-12-05 12:52:16 +0000

    updated identation

--- lib/piggy_bank.rb
+++ lib/piggy_bank.rb
@@ -1,15 +1,15 @@
 class PiggyBank
-def initialize
-  @input_coins = 0 # creating a variable that is going across the methods
-end
 
+  def initialize
+      @input_coins = 0 # creating a variable that is going across the methods
+  end
 
   def insert(sum)
       @input_coins = @input_coins + sum
-
   end
+
   def count
-    @input_coins
+      @input_coins
   end
 
   def shake
@@ -17,6 +17,6 @@ end
   end
 
   def break
-@input_coins = 5
+    @input_coins = 5
   end
 end
--- spec/piggy_bank_spec.rb
+++ spec/piggy_bank_spec.rb
@@ -2,29 +2,33 @@ require 'piggy_bank'
 
 describe 'PiggyBank' do
   it 'takes coins and keeps saved in a piggy bank'do
-    piggy_bank = PiggyBank.new
-    expect(piggy_bank).to be_instance_of(PiggyBank)
+        piggy_bank = PiggyBank.new
+        expect(piggy_bank).to be_instance_of(PiggyBank)
   end
+
   it 'insert coins in a piggy bank'do
-    piggy_bank = PiggyBank.new
-    piggy_bank.insert(0)
+        piggy_bank = PiggyBank.new
+        piggy_bank.insert(0)
     expect(piggy_bank).to respond_to(:insert).with(1).argument
   end
+
   it 'takes 5 coins'do
-    piggy_bank = PiggyBank.new
-    piggy_bank.insert(5)
+      piggy_bank = PiggyBank.new
+      piggy_bank.insert(5)
     expect(piggy_bank.count).to eq(5)
   end
+
   it 'can check if there is coins inside'do
     piggy_bank = PiggyBank.new
     piggy_bank.shake
     expect(piggy_bank.shake).to eq('cling')
   end
+
   it 'can break and have the coins saved'do
   piggy_bank = PiggyBank.new
   piggy_bank.break
   expect(piggy_bank.break).to eq(5)
-end 
+end
 
 end
 
